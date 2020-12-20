5fdab42bba98b630321dc140d3107dbf7557f908
skarnata <setiawankarnatachan@gmail.com>
2020-12-20 16:56:44 +0700

    Modifikasi urls, views, post_list dan post_detail

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -6,5 +6,10 @@ from django.utils import timezone
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    context = { 'posts': posts}
+    context = {'posts': posts}
     return render(request, 'blog/post_list.html', context)
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
